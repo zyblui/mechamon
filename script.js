74c2303fb8074548e4b274c5b38a6a920a4bcc9e
y.b.l** <zyblui@outlook.com>
2025-06-28 23:13:37 +0800

    update

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ function render() {
     document.getElementById("p2Pokemon").style.backgroundImage = "url('front/" + players[1].build[0].name + ".png')"
 }
 render();
+function capitalize(str) {
+    let temp = str.split(" ");
+    for (let i = 0; i < temp.length; i++) {
+        temp[i] = temp[i][0].toUpperCase() + temp[i].slice(1)
+    }
+    temp = temp.join(" ");
+    temp = temp.split("-");
+    for (let i = 0; i < temp.length; i++) {
+        temp[i] = temp[i][0].toUpperCase() + temp[i].slice(1)
+    }
+    temp = temp.join("-");
+    return temp;
+}
+for (let i of pokemon) {
+    let div = document.createElement("div");
+    div.classList.add("listButton");
+    div.innerHTML = capitalize(i.name);
+    div.addEventListener("click", function () {
+        document.querySelector(".pokemon-select.selected").innerText = capitalize(i.name)
+    })
+    document.getElementById("pokemonList").appendChild(div);
+}
+for (let i of document.getElementsByClassName("pokemon-select")) {
+    i.addEventListener("click", function () {
+        document.querySelector(".pokemon-select.selected")?.classList.remove("selected");
+        i.classList.add("selected");
+        document.getElementById("pokemonList").classList.add("show")
+    })
+}
