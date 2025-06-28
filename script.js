5c96fd86c54d8722425f7d4a9ca1c23da58f7d63
y.b.l** <zyblui@outlook.com>
2025-06-28 22:18:24 +0800

    update

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let players=[{
-    "name":"Player 1"
-},{
-    "name":"Player 2",
-    "build":[{
-        "name":"charizard"
+let players = [{
+    "name": "Player 1",
+    "build": [{
+        "name": "pikachu"
+    }]
+}, {
+    "name": "Player 2",
+    "build": [{
+        "name": "charizard"
     }]
 }]
+function render() {
+    document.getElementById("p1Pokemon").style.backgroundImage = "url('back/" + players[0].build[0].name + ".png')"
+    document.getElementById("p2Pokemon").style.backgroundImage = "url('front/" + players[1].build[0].name + ".png')"
+}
+render();
